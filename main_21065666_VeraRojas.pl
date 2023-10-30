5327d82454e78f592ad1fdc2baaef3ddbd0e28f5
Agustin Vera <agustin.vera@usach.cl>
2023-10-29 23:26:03 -0300

    Se agrega el RF2 en conjunto con el archivo Main

---
+++ main_21065666_VeraRojas.pl
@@ -0,0 +1,9 @@
+
+%#################################################################
+%             Main
+%#################################################################
+
+
+% RF 2 - TDA Option - Constructor
+option(Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword, [Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword]).
+
