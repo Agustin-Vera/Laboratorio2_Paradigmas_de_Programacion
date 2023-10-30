fca623df3c1e5a4fefab67a5526a454a6fa559f3
Agustin Vera <agustin.vera@usach.cl>
2023-10-30 20:52:10 -0300

    Se agrega el RF3 en conjunto con el archivo TDA Option, se agregan distintos predicados y reglas complementarias

--- main_21065666_VeraRojas.pl
+++ main_21065666_VeraRojas.pl
@@ -1,3 +1,5 @@
+:-include(tda_option_21065666_VeraRojas).
+
 
 %#################################################################
 %             Main
@@ -5,5 +7,16 @@
 
 
 % RF 2 - TDA Option - Constructor
-option(Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword, [Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword]).
+option(Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword, [Code, Message, ChatbotCodeLink, InitialFlowCodeLink, LowerKeywords]) :-
+    keywordsToLowerKeywords(Keyword, LowerKeywords).
 
+
+% RF 3 - TDA Flow - Constructor
+flow(Id, NameMsg, Option, [Id, NameMsg, Option]) :-
+    uniqueOptions(Option).
+
+/*
+option(1, "1) Viajar", 1, 1, ["viajar", "turistear", "conocer"], O1), 
+option(2, "2) Estudiar", 2, 1, ["aprender", "perfeccionarme"], O2), 
+flow(1, "Flujo Principal Chatbot 1\nBienvenido\n¿Qué te gustaría hacer?", [O1, O2], F10). 
+*/
---
+++ tda_option_21065666_VeraRojas.pl
@@ -0,0 +1,42 @@
+
+%#################################################################
+%             TDA Option
+%#################################################################
+% Code X Message X ChatbotCodeLink X InitialFlowCodeLink X Keyword*
+
+% Constructor
+%El constructor queda definido en el archivo main
+
+
+% Selectores
+getOptionCode([Code, _, _, _, _], Code).
+
+
+% dada una lista de options obtiene todos los CODES
+getOptionsCodes([], []).
+getOptionsCodes([Option|Resto], [Code|Resultado]) :-
+    getOptionCode(Option, Code),
+    getOptionsCodes(Resto, Resultado), !.
+
+
+% Pertenencias
+
+% verifica que dada una lista de optionCODES, no se repita el CODE de ninguna
+codeExist([]).
+codeExist([Code|Resto]) :-
+    not(member(Code, Resto)),   % Code no está en el resto de la lista
+    codeExist(Resto).
+
+% Verifica si hay un code repetido en una lista de codes
+uniqueOptions(Options) :-
+    getOptionsCodes(Options, OptionsCodes),
+    codeExist(OptionsCodes).
+
+
+% Otros predicados
+
+% keywordsToLowerKeywords(Keywords, NewKeywords)
+keywordsToLowerKeywords([], []).
+keywordsToLowerKeywords([Keyword|Resto], [LowerKeyword|Resultado]) :-
+    string_lower(Keyword, LowerKeyword),
+    keywordsToLowerKeywords(Resto, Resultado).
