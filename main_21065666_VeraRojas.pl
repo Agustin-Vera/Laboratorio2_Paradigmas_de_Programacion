7d436adee8ee5e0fc356dde24326ece5f9f00c7e
Agustin Vera <agustin.vera@usach.cl>
2023-11-07 22:59:15 -0300

    Se agrega el RF10, con todos los predicados que lo complementan, no se agrega documentacion y finalmente se agrega el scrpt de pruebas, falta agregar mas ejemplos

--- main_21065666_VeraRojas.pl
+++ main_21065666_VeraRojas.pl
@@ -121,15 +121,11 @@ systemAddUser(System, User, NewSystem) :-
     setSystemChatHistorys(NewSystemUsers, NewChatHistorys, NewSystem), !.
 systemAddUser(System, _, System).
 
-/*
-set_prolog_flag(answer_write_options,[max_depth(0)]).
-option(1, "1) Viajar", 1, 1, ["viajar", "turistear", "conocer"], O1), 
-option(2, "2) Estudiar", 2, 1, ["aprender", "perfeccionarme"], O2), 
-flow(1, "Flujo Principal Chatbot 1\nBienvenido\n¿Qué te gustaría hacer?", [O1], F10),
-flowAddOption(F10, O2, F11),
-chatbot(0, "Asistente", "Bienvenido\n¿Que te gustaria hacer?", 1, [], CB0),
-chatbotAddFlow(CB0, F11, CB01),
-system("Chatbots Paradigmas", 0, [], S0),
-systemAddChatbot(S0, CB01, S1),
-systemAddUser(S1, "User1", S2).
-*/
+% RF 10 - TDA System
+systemLogin(System, User, NewSystem) :-
+    getSystemUsers(System, SystemUsers),
+    userExist(SystemUsers, User),
+    not(usersLogged(SystemUsers)),
+    changeUsersStatus(SystemUsers, User, 1, NewSystemUsers),
+    setSystemUsers(System, NewSystemUsers, NewSystem), !.
+systemLogin(System, _, System).
--- tda_user_21065666_VeraRojas.pl
+++ tda_user_21065666_VeraRojas.pl
@@ -16,6 +16,11 @@ getUsersIDs([User|Resto], [Name|Resultado]) :-
     getUserName(User, Name),
     getUsersIDs(Resto, Resultado), !.
 
+getUsersStatus([],[]).
+getUsersStatus([User|Resto], [Status|Resultado]) :-
+    getUserStatus(User, Status),
+    getUsersStatus(Resto, Resultado), !.
+    
 % Pertenecias
 
 % verifica si un user existe en una lista de users
@@ -23,3 +28,23 @@ userExist(Users, Username) :-
     getUsersIDs(Users, UserNames),
     string_lower(Username, NewUsername),
     member(NewUsername, UserNames).
+
+% verifica si hay usuarios iniciados
+usersLogged(Users) :-
+    getUsersStatus(Users, UsersStatus),
+    member(1, UsersStatus).
+
+
+% Modificadores
+
+changeUserStatus(User, Status, UpdatedUser) :-
+    getUserName(User, Name),
+    user(Name, Status, UpdatedUser).
+
+changeUsersStatus([], _, _, []).
+changeUsersStatus([User|Resto], Username, Status, [UserLogged|Resto]) :-
+    string_lower(Username, UsernameLower),
+    member(UsernameLower, User),
+    changeUserStatus(User, Status, UserLogged), !.
+changeUsersStatus([User|Resto], Username, Status, [User|Resultado]) :-
+    changeUsersStatus(Resto, Username, Status, Resultado).
